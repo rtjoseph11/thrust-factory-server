ac0af14c02ba51d8edf5119c1ab1ceb1c2cc5a8f
Sachin Goel <sachingoel@gmail.com>
2013-09-28 16:17:43 -0700

    using airplane instead of box

--- public/src/entities.js
+++ public/src/entities.js
@@ -4,12 +4,16 @@
             color: "blue",
             x: THRUSTER.Helpers.getRandomInt(0, THRUSTER.Config.CANVAS_WIDTH),
             y: 0,
-            width: 32,
-            height: 32,
+            init: function() {
+                this.plane = new Image();
+                this.plane.src = '/public/images/LeftWingGold.png';
+            },
             draw: function() {
-                THRUSTER.ctx.fillStyle = this.color;
-                THRUSTER.ctx.fillRect(this.x, this.y, this.width, this.height);
+                if (this.plane.complete) {
+                    THRUSTER.ctx.drawImage(this.plane, this.x, this.y)
+                }
             }
         }
-    }
+    };
+    THRUSTER.Entities.player.init();
 })(THRUSTER);
